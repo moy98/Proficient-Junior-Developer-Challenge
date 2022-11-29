ae245f642639018bf1fb102db7d51c790559ec13
MMedinaEscolar <moi98.medina@gmail.com>
2022-11-29 11:13:38 -0600

    Pagina Funcional falntando css de boton de calcular y reset

--- src/assets/context/Data.js
+++ src/assets/context/Data.js
@@ -1,12 +1,15 @@
 
 import React, { useState } from 'react'
 
+
 var Tvalue = 0
 var Tmeters = 0
 var Tsubtotal = 0
 var Ttax = 0
 var Ttotal = 0
 var Tduetoday = 0
+
+
 
 const Data = (props) => {
 
@@ -18,26 +21,23 @@ const Data = (props) => {
                     <img src={props.image} className='card-img' />
                     <h3>{props.title}</h3>
                 </div>
-                <div>
-                    <button onClick={() => {
-                        setCounter(counter - 1)
-                        Tvalue--
-                        Tmeters = Tmeters - props.meters
-                        Tsubtotal = Tmeters * 200
-                        Ttax = Tsubtotal * 1.16
-                        Ttotal = Tsubtotal + Ttax
-                        Tduetoday = Ttotal * 0.5
+                <div className='List-btn'>
+                    <button className='Restbtn' onClick={() => {
+                        if (counter <= 0) {
+                            setCounter(counter = 0)
+                        } else {
+                            setCounter(counter - 1)
+                            Tvalue--
+                            Tmeters = Tmeters - props.meters
+                        }
 
                     }}>-</button>
-                    <input disabled value={counter}></input>
-                    <button onClick={() => {
+                    <input disabled className='quantity' value={counter}></input>
+                    <button className='Addbtn' onClick={() => {
                         setCounter(counter + 1)
                         Tvalue++
                         Tmeters = Tmeters + props.meters
-                        Tsubtotal = Tmeters * 200
-                        Ttax = Tsubtotal * 0.16
-                        Ttotal = Tsubtotal + Ttax
-                        Tduetoday = Ttotal * 0.5
+
 
                     }
                     } >+</button>
@@ -57,34 +57,41 @@ export const ComponentCost = () => {
     let [tax, setTax] = useState(0)
     let [total, setTotal] = useState(0)
     let [dueToday, setDuetoday] = useState(0)
-    return (<div>
+    return (<div className='App-SummaryCost' >
         <section className='App-btn'>
-        <button onClick={() => { }}>Clear</button>
-        <button onClick={() => {
+            <button onClick={() => {
+                document.location.reload()
 
+            }}>Clear</button>
+            <button onClick={() => {
 
-            setCounter(counter = Tvalue)
-            setMeterCounter(meterCounter = Tmeters.toFixed(2))
-            setSubtotal(subtotal = Tsubtotal.toFixed(2))
-            setTax(tax = Ttax.toFixed(2))
-            setTotal(total = Ttotal.toFixed(2))
-            setDuetoday(dueToday = Tduetoday.toFixed(2))
+                Tsubtotal = Tmeters * 200
+                Ttax = Tsubtotal * 0.16
+                Ttotal = Tsubtotal + Ttax
+                Tduetoday = Ttotal * 0.5
 
-        }}>Calculate</button>
+                setCounter(counter = Tvalue)
+                setMeterCounter(meterCounter = Tmeters.toFixed(2))
+                setSubtotal(subtotal = Tsubtotal.toFixed(2))
+                setTax(tax = Ttax.toFixed(2))
+                setTotal(total = Ttotal.toFixed(2))
+                setDuetoday(dueToday = Tduetoday.toFixed(2))
+
+            }}>Calculate</button>
         </section>
         <section>
             <h3 className='App-Summary'>Summary</h3>
         </section>
 
-        <div className='App-cost'>
+        <div >
 
-            <ul>
-                <li>Total Items <input disabled value={counter}></input></li>
-                <li>Total M2<input disabled value={meterCounter}></input></li>
-                <li>Subtotal<input disabled value={subtotal}></input></li>
-                <li>Tax<input disabled value={tax}></input></li>
-                <li>Total<input disabled value={total}></input></li>
-                <li>Due Today 50%<input disabled value={dueToday}></input></li>
+            <ul className='App-cost'>
+                <li className='App-Costlist'>Total Items <input disabled value={counter}></input></li>
+                <li className='App-Costlist' >Total M2<input disabled value={meterCounter}></input></li>
+                <li className='App-Costlist' >Subtotal<input disabled value={subtotal}></input></li>
+                <li className='App-Costlist' >Tax<input disabled value={tax}></input></li>
+                <li className='App-Costlist' ><p>Total</p><input disabled value={total}></input></li>
+                <li className='App-Costlist' ><p> Due Today 50% </p><input disabled value={dueToday}></input></li>
             </ul>
 
 
